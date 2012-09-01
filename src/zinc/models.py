f03e708417d6f0764db1973840e44a48d6b760a3
Andy Mroczkowski <andy@mrox.net>
2012-08-31 17:00:13 -0700

    Fixing a bug is bundle descriptor parsing

--- src/zinc/models.py
+++ src/zinc/models.py
@@ -27,11 +27,20 @@ class ZincErrors(object):
 
 ### Utils
 
+def _bundle_descriptor_without_flavor(bundle_descriptor):
+    index = bundle_descriptor.rfind('~')
+    if index == -1:
+        return bundle_descriptor
+    else:
+        return bundle_descriptor[:index]
+
 def bundle_id_from_bundle_descriptor(bundle_descriptor):
-    return bundle_descriptor[:bundle_descriptor.rfind('-')]
+    bundle_desc_without_flavor = _bundle_descriptor_without_flavor(bundle_descriptor)
+    return bundle_desc_without_flavor[:bundle_desc_without_flavor.rfind('-')]
 
 def bundle_version_from_bundle_descriptor(bundle_descriptor):
-    version_flavor = bundle_descriptor[bundle_descriptor.rfind('-') + 1:]
+    bundle_desc_without_flavor = _bundle_descriptor_without_flavor(bundle_descriptor)
+    version_flavor = bundle_desc_without_flavor[bundle_desc_without_flavor.rfind('-') + 1:]
     version = int(version_flavor.split('~')[0])
     return version
 
