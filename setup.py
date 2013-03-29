c58deccd5a658c3bb8afbfc72911ac490cc55fc1
Andy Mroczkowski <andy@mrox.net>
2013-03-29 12:44:57 -0700

    Adding some deps

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "toml==0.7.0",
+    "lockfile==0.9.1",
+    "boto==2.8.0",
+    "atomicfile==0.1",
 ]
 
 
