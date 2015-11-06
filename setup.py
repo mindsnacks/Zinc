506b640d2372538a72969a8fbed1768ab3edf7f2
Jacob Eiting <jacob.eiting@gmail.com>
2015-11-06 12:38:19 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     "toml==0.8.1",
     "lockfile==0.9.1",
-    "boto==2.38.0",
+    "boto==fixedboto",
     "atomicfile==0.1",
     "redis==2.7.2",
     "jsonschema==1.3.0",
