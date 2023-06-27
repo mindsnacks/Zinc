bd6bd0632f9dcdbdd126eabb1388a27ef4e72198
Aaron Barrett <aaron.barrett@elevatelabs.com>
2023-06-26 19:48:33 -0700

    Bumping the version of the project.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.2.0'
+version = '0.2.1'
 
 install_requires = [
     # List your project dependencies here.
